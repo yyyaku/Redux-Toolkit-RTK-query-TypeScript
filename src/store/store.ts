eaad342d8e7cf9a181e4ce78777c28c0df61d5d0
Артемий <artema121rus@mail.ru>
2024-04-28 12:11:58 +0300

    Создает кастомные хуки для redux

---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+
+export const setupStore = () => {
+    return configureStore({ reducer: rootReducer });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppState = ReturnType<typeof setupStore>;
+export type AppDispatch = AppState["dispatch"];
