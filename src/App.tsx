5a7835731688f454525930159d2d0d23c3dff208
Артемий <artema121rus@mail.ru>
2024-04-28 14:22:59 +0300

    Добавляет обработку стандартого сценария

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import { userSlice } from "./store/reducers/UserSlice";
+import { useAppDispatch, useTypedSelector } from "./hooks/redux";
 
 function App() {
-    return <div className='App'></div>;
+    // вытаскиваем ActionCreatorы из слайса с помощью actions
+    const { increment } = userSlice.actions;
+    const dispatch = useAppDispatch();
+    const { count } = useTypedSelector((state) => state.userReducer);
+    return (
+        <div className='App'>
+            <h1>{count}</h1>
+            <button onClick={() => dispatch(increment(10))}>incrtment</button>
+        </div>
+    );
 }
 
 export default App;
